18bd3e187e4a6e36c41916f87c981c77cb1191e6
ifeanyi_ns <ifeanyi_ns@icloud.com>
2023-09-28 09:52:30 +0100

    theme addition

--- src/index.js
+++ src/index.js
@@ -6,6 +6,19 @@ const menuIcon = document.querySelector('.hamburgerMenu')
 menuIcon.addEventListener('click', ()=>{
     const leftPanel = document.querySelector('.leftPanel');
     leftPanel.classList.toggle("hidden");
-    // const rightPanel = document.querySelector('.rightPanel');
-    // rightPanel.style.width('100%')
+    
+})
+
+//on start up checked wether its on light mode or dark mode
+const checkbox = document.getElementById("inputCbox");
+if(checkbox.checked === true){
+    document.body.classList.add("light");
+}
+else{
+    document.body.classList.remove("light");
+}
+
+//event listener for every time the dark mode toggle change
+checkbox.addEventListener("change", () =>{
+    document.body.classList.toggle("light");
 })
